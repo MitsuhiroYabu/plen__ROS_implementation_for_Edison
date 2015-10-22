b7390790022975c071cef75b08c214ed51819419
MitsuhiroYabu <mitsuhirooooo@gmail.com>
2015-10-22 23:20:08 +0900

    GPIOinterupttest

--- LEDblink.py
+++ LEDblink.py
@@ -2,24 +2,50 @@
 import mraa
 import time
 
+
+class Counter:
+    def __init__(self):
+        pass
+
+    count = 0
+
+
+c = Counter()
+
+
+def GPIOInterupt():
+    print("interupt now")
+    c.count += 1
+    if c.count >= 10:
+        x.write(0)
+    elif c.count >= 15:
+        c.count = 0
+        x.write(0)
+
+
 # Setup
 print("mraa Version: %s" % mraa.getVersion())
 x = mraa.Gpio(8)
+inputGPIO = mraa.Gpio(12)
 x.dir(mraa.DIR_OUT)
-pwm = mraa.Pwm(3)
-pwm.period_us(700)
-pwm.enable(True)
+inputGPIO.dir(mraa.DIR_IN)
+inputGPIO.isr(mraa.EDGE_BOTH, GPIOInterupt, GPIOInterupt)
+pwm0 = mraa.Pwm(3)
+pwm0.period_us(700)
+pwm0.enable(True)
+pwm1 = mraa.Pwm(5)
+pwm1.period_us(700)
+pwm1.enable(True)
 value = 0.0
 
 # Loop
 while True:
-    x.write(0)
     print "ON"
     time.sleep(0.5)
-    x.write(0)
     print "OFF"
     time.sleep(0.5)
-    pwm.write(value)
-    value = value + 0.05
+    pwm0.write(value)
+    pwm1.write(value)
+    value += 0.05
     if value >= 1:
-    	value = 0.0
+        value = 0.0
