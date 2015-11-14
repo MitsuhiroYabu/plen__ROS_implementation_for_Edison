17ae68b455418e6ec04bb6df837a00f12fa1fab5
MitsuhiroYabu <mitsuhirooooo@gmail.com>
2015-11-14 14:15:11 +0900

    ble_fix

--- script/bleNode.py
+++ script/bleNode.py
@@ -12,9 +12,9 @@ moji_2 = []
 result = []
 detect = 0
 for line in end_of_pipe:
-        if line.find('bluetoothd') != -1:
-                moji_2 = line.split(' ')
-                detect=1
+    if line.find('bluetoothd') != -1:
+        moji_2 = line.split(' ')
+        detect=1
 
 val2 = 0
 if detect == 1:
@@ -247,72 +247,6 @@ class Descriptor(dbus.service.Object):
         print('Default WriteValue called, returning error')
         raise NotSupportedException()
 
-class BatteryService(Service):
-    """
-    Fake Battery service that emulates a draining battery.
-
-    """
-    BATTERY_UUID = '180f'
-
-    def __init__(self, bus, index):
-        Service.__init__(self, bus, index, self.BATTERY_UUID, True)
-        self.add_characteristic(BatteryLevelCharacteristic(bus, 0, self))
-
-
-class BatteryLevelCharacteristic(Characteristic):
-    """
-    Fake Battery Level characteristic. The battery level is drained by 2 points
-    every 5 seconds.
-
-    """
-    BATTERY_LVL_UUID = '2a19'
-
-    def __init__(self, bus, index, service):
-        Characteristic.__init__(
-                self, bus, index,
-                self.BATTERY_LVL_UUID,
-                ['read', 'notify'],
-                service)
-        self.notifying = False
-        self.battery_lvl = 100
-        gobject.timeout_add(5000, self.drain_battery)
-
-    def notify_battery_level(self):
-        if not self.notifying:
-            return
-        self.PropertiesChanged(
-                GATT_CHRC_IFACE,
-                { 'Value': [dbus.Byte(self.battery_lvl)] }, [])
-
-    def drain_battery(self):
-        if self.battery_lvl > 0:
-            self.battery_lvl -= 2
-            if self.battery_lvl < 0:
-                self.battery_lvl = 0
-        #print('Battery Level drained: ' + repr(self.battery_lvl))
-        self.notify_battery_level()
-        return True
-
-    def ReadValue(self):
-        print('Battery Level read: ' + repr(self.battery_lvl))
-        return [dbus.Byte(self.battery_lvl)]
-
-    def StartNotify(self):
-        if self.notifying:
-            print('Already notifying, nothing to do')
-            return
-
-        self.notifying = True
-        self.notify_battery_level()
-
-    def StopNotify(self):
-        if not self.notifying:
-            print('Not notifying, nothing to do')
-            return
-
-        self.notifying = False
-
-
 class TestService(Service):
     """
     Dummy test service that provides characteristics and descriptors that
@@ -490,9 +424,6 @@ def main():
             signal_name="PropertiesChanged",
             path_keyword="path")
 
-    service_manager.RegisterService(bat_service.get_path(), {},
-                                    reply_handler=register_service_cb,
-                                    error_handler=register_service_error_cb)
     service_manager.RegisterService(test_service.get_path(), {},
                                     reply_handler=register_service_cb,
                                     error_handler=register_service_error_cb)
